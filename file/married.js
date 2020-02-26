5ea1d96442af33739c1db539a09c82b33cc2a2ad
watcom2003 <watcom@hotmail.com>
2020-02-26 13:55:36 +0700

    Update married.js

--- file/married.js
+++ file/married.js
@@ -2,7 +2,7 @@ function married(x , y) {
         var family = [ {"husband":"jack","wife":"jill"}, {"husband":"homer","wife":"marge"},{"husband":"bill","wife":"ben"} ]
         var marry = false;
        
-        for (i=0; i< family.length-1; i++) {
+        for (i=0; i< family.length; i++) {
               if ((x==family[i].husband) && (y==family[i].wife)) { marry = true}
         }
         return(marry)
