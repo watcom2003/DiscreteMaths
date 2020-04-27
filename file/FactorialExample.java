6a2698de7155a07765757d0836e42521793148c2
watcom2003 <watcom@hotmail.com>
2020-04-27 17:01:50 +0700

    Create FactorialExample.java

---
+++ file/FactorialExample.java
@@ -0,0 +1,23 @@
+public class FactorialExample{  
+ public static long factorialLoop(int n) {
+     long res = 1;
+     if (n>1)
+        for (int i=2;i<=n;i++) res *= i;
+    return res;
+ }    
+ public static long factorialRecursive(int n) {
+     if (n==1) return 1; else
+        return n * factorialRecursive(n-1);
+ }
+    
+ public static void main(String args[]){  
+  int number = 500000;
+  System.out.println("Factorial of "+number+" is: "+factorialLoop(number));    
+  try {
+    System.out.println("Factorial of "+factorialRecursive(number));
+  }
+  catch(Exception error) {
+    System.out.println(error.getMessage());
+  }
+}
+}  
